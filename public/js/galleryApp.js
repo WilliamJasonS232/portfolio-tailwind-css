4c521c1c4d9875763d2739cbe123a56afc3ec32e
WilliamJasonS232 <williamjason2828@gmail.com>
2024-06-16 20:27:29 +0700

    portfolio tailwind css

--- public/js/galleryApp.js
+++ public/js/galleryApp.js
@@ -1,4 +1,4 @@
-import createModal from '/public/js/createModal.js'
+import createModal from './public/js/createModal.js'
 createModal();
 
 const modalableImages = document.querySelectorAll('[data-modal="true"]');
